6e19f072a422b613b9816dce47e7359ea969b3be
Anthony Sainez <tsainez@gmail.com>
2018-09-28 14:16:36 -0700

    init

---
+++ README.md
@@ -0,0 +1,28 @@
+# Björt
+
+## Start
+> _Now that I'm here_, what do I do?
+
+First, check that you've done these things:
+
+* Check classroom, or the resource library.
+* Cloned the repository using [Github Desktop](https://desktop.github.com/).
+* Added it as a project on [Android Studio](https://www.firstinspires.org/sites/default/files/uploads/resource_library/ftc/android-studio-tutorial.pdf).
+
+Next, open up the folder called _ftc_app-3.5_, and you will find further instructions in there. 
+
+Now you can start coding!
+
+## FAQ
+> I'm having trouble finding _where_ the code is! 
+
+Try this directory:
+`ftc_app-3.5\TeamCode\src\main\java\org\firstinspires\ftc\teamcode`
+This is where the majority of our coding work takes place. Then take a look at the readme file.
+
+> I'm finished with my work, how do I get it to update on GitHub?
+
+Push your commits to origin by clicking the buttons along the top of the Github desktop application.
+
+## End
+Any questions? Email us at <mailto:gecarobotics@gmail.com>
---
+++ ftc_app-3.5/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -0,0 +1,177 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+// Color Sensor Imports
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+/**
+ * Created by tsain on 12/2/2017(?)
+ * Based off of modified BasicOpMode_Linear.java
+ * Added Color Sensor on 12/2/2017
+ **/
+
+
+@TeleOp(name="TeleOpMode", group="Linear Opmode")
+//@Disabled
+public class TeleOpMode extends LinearOpMode {
+
+    // Movement
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotor motorTest;
+
+    // Servo
+    Servo servo;
+    double servoPosition = 0.0;
+
+    // Sensors
+    ColorSensor colorSensor;    // Hardware Device Object
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+
+        // *********MOTORS*********
+            // main motors
+        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
+        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        motorTest = hardwareMap.get(DcMotor.class, "motorTest");
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // *********SERVO*********
+            // servo for manipulator
+        servo = hardwareMap.servo.get("servo");
+        servo.setPosition(servoPosition);
+
+        // *********SENSORS*********
+            // color sensor
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+        float hsvValues[] = {0F,0F,0F};
+        // values is a reference to the hsvValues array.
+        final float values[] = hsvValues;
+        // get a reference to the RelativeLayout so we can change the background
+        // color of the Robot Controller app to match the hue detected by the RGB sensor.
+        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+        // bPrevState and bCurrState represent the previous and current state of the button.
+        boolean bPrevState = false;
+        boolean bCurrState = false;
+        // bLedOn represents the state of the LED.
+        boolean bLedOn = true;
+        // get a reference to our ColorSensor object.
+        colorSensor = hardwareMap.get(ColorSensor.class, "sensor_color");
+        // Set the LED in the beginning
+        colorSensor.enableLed(bLedOn);
+
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Basic Movement
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower;
+            double rightPower;
+
+            // Choose to drive using either Tank Mode, or POV Mode
+            // Comment out the method that's not used.  The default below is POV.
+
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+            double drive = -gamepad1.left_stick_y;
+            double turn  =  gamepad1.right_stick_x;
+            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+            // Tank Mode uses one stick to control each wheel.
+            // - This requires no math, but it is hard to drive forward slowly and keep straight.
+            // leftPower  = -gamepad1.left_stick_y ;
+            // rightPower = -gamepad1.right_stick_y ;
+
+            // Moving the servo
+            // Left and right triggers are used to close or open the claw, respectively
+            double openness = 0.0;
+            double servoPosition = openness;
+            if (gamepad1.right_trigger != 0 || gamepad1.left_trigger != 0) {
+                if (gamepad1.left_trigger > 0) {
+                    openness = openness - gamepad1.left_trigger;
+                } else {
+                    openness = openness + gamepad1.right_trigger;
+                }
+            }
+
+
+            // Send calculated power to wheels
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+
+            // Color Sensor
+
+            // check the status of the x button on either gamepad.
+            bCurrState = gamepad1.x;
+
+            // check for button state transitions.
+            if (bCurrState && (bCurrState != bPrevState))  {
+
+                // button is transitioning to a pressed state. So Toggle LED
+                bLedOn = !bLedOn;
+                colorSensor.enableLed(bLedOn);
+            }
+
+            // update previous state variable.
+            bPrevState = bCurrState;
+
+            // convert the RGB values to HSV values.
+            Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
+
+            // Show the elapsed game time and wheel power, as well as stuff for color sensor.
+            telemetry.addData("LED", bLedOn ? "On" : "Off");
+            telemetry.addData("Clear", colorSensor.alpha());
+            telemetry.addData("Red  ", colorSensor.red());
+            telemetry.addData("Green", colorSensor.green());
+            telemetry.addData("Blue ", colorSensor.blue());
+            telemetry.addData("Hue", hsvValues[0]);
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+
+            // change the background color to match the color detected by the RGB sensor.
+            // pass a reference to the hue, saturation, and value array as an argument
+            // to the HSVToColor method.
+            relativeLayout.post(new Runnable() {
+                public void run() {
+                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+                }
+            });
+
+            telemetry.update();
+        }
+    }
+}
